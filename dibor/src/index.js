3e302bbde257a8faa1773b5254ec79ec6771ec8d
boogi590 <simonfraiberg@gmail.com>
2023-05-23 22:33:53 +0300

    changed back the app fucntion in index

--- dibor/src/index.js
+++ dibor/src/index.js
@@ -2,12 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Chat from './chat/Chat';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Chat/>
+    <App/>
   </React.StrictMode>
 );
 
