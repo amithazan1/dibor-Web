8ca365803380ed909a4cc1e9f86b37a50065360b
amithazan1 <amith246@gmail.com>
2025-04-09 18:03:53 +0300

    deployment

--- dibor/backend/server.js
+++ dibor/backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+const path = require("path");
 
 const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
@@ -28,115 +29,13 @@ app.use("/api/message", messageRoute);
 app.use("/api/chat", chatRoute);
 app.use("/api/user", userRoute);
 
+app.use(express.static(path.join(__dirname, "..", "frontend", "dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "frontend", "dist", "index.html"));
+});
+
 server.listen(PORT, () => {
   connectToMongoDB();
   console.log("Server listening in port", PORT);
 });
-
-/** 
- * 
- * 
- * const bodyParser = require("body-parser");
-const cors = require("cors");
-const mongoose = require("mongoose");
-require("custom-env").env(process.env.NODE_ENV, "./config");
-mongoose.connect(process.env.CONNECTION_STRING, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-app.use(express.static("public"));
-app.use(cors());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.json());
-
-app.get("*", (req, res) => {
-  // Redirect to the home page or any other desired URL
-  res.redirect("/");
-});
-
-//socket io
-const http = require("http");
-const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server, {
-  cors: {
-    origin: "http://localhost:12345",
-    methods: ["GET", "POST"],
-  },
-});
-
-io.on("connection", (socket) => {
-  // Create an object to store the active chat rooms and the users who have joined each room
-  const activeChatRooms = {};
-
-  socket.on("enterChat", (data) => {
-    const roomId = data;
-    const userId = socket.id;
-
-    // Check if the room exists in the activeChatRooms object
-    if (activeChatRooms[roomId]) {
-      // Check if the user has already joined the room
-      if (activeChatRooms[roomId].includes(userId)) {
-        return;
-      }
-
-      // Add the user to the room's user list
-      activeChatRooms[roomId].push(userId);
-    } else {
-      // Create a new room and add the user to the user list
-      activeChatRooms[roomId] = [userId];
-    }
-
-    // Join the room
-    socket.join(roomId);
-  });
-
-  socket.on("enterUserChat", (data) => {
-    const roomId = data;
-    const userId = socket.id;
-
-    // Check if the room exists in the activeChatRooms object
-    if (activeChatRooms[roomId]) {
-      // Check if the user has already joined the room
-      if (activeChatRooms[roomId].includes(userId)) {
-        return;
-      }
-
-      // Add the user to the room's user list
-      activeChatRooms[roomId].push(userId);
-    } else {
-      // Create a new room and add the user to the user list
-      activeChatRooms[roomId] = [userId];
-    }
-
-    // Join the room
-    socket.join(roomId);
-  });
-
-  socket.on("sendMessage", (data) => {
-    socket.to(data.chatRoom).emit("recivedMessage", data.chatRoom);
-    socket.to(data.userRoom).emit("recivedMessageAlert", {
-      from: data.from,
-      message: data.message,
-      time: data.time,
-    });
-  });
-
-  socket.on("deleteUserChat", (data) => {
-    socket.to(data.username).emit("deleteChat", data);
-  });
-  socket.on("disconnect", () => {
-    const rooms = Object.keys(socket.rooms);
-    rooms.forEach((room) => {
-      if (room !== socket.id) {
-        socket.leave(room);
-      }
-    });
-  });
-});
-
-server.listen(3001, () => {
-  console.log("Socket.IO server listening on port 3001");
-});
-*/
